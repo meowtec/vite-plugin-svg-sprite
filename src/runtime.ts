f5ab2be26c42b46aad89bb32d143c0c0eaec7123
meowtec <bertonzh@gmail.com>
2021-03-15 11:06:19 +0800

    refactor(runtime): rewrite insert and remove domready

--- src/runtime.ts
+++ src/runtime.ts
@@ -1,30 +1,25 @@
-import domReady from 'domready';
-
-const doc = document;
-
 const idSet = (window as any)._SVG_SPRITE_IDS_ = (window as any)._SVG_SPRITE_IDS_ || [];
 
-let root: SVGSVGElement | null = null;
+const root = document.createElementNS('http://www.w3.org/2000/svg', 'svg') as SVGSVGElement;
+root.style.position = 'absolute';
+root.style.width = '0';
+root.style.height = '0';
 
-function getSVGRoot() {
-  if (!root) {
-    root = document.createElementNS('http://www.w3.org/2000/svg', 'svg') as SVGSVGElement;
-    root.style.position = 'absolute';
-    root.style.width = '0';
-    root.style.height = '0';
-    doc.body.insertBefore(root, doc.body.firstChild);
-  }
+function insertRoot() {
+  document.body.insertBefore(root, document.body.firstChild);
+}
 
-  return root;
+if (document.readyState === 'loading') {
+  document.addEventListener('DOMContentLoaded', insertRoot);
+} else {
+  insertRoot();
 }
 
 export default function addSymbol(symbol: string, id: string) {
-  if (idSet.indexOf(id) > -1 || doc.getElementById(id)) {
+  if (idSet.indexOf(id) > -1 || document.getElementById(id)) {
     console.warn(`Icon #${id} was duplicately registered. It must be globally unique.`);
   }
   idSet.push(id);
 
-  domReady(() => {
-    getSVGRoot().insertAdjacentHTML('beforeend', symbol);
-  });
+  root.insertAdjacentHTML('beforeend', symbol);
 }
